09443608fac651bea066d6272455ef9d7e33f208
chenqian <1173006527@qq.com>
2017-02-08 11:04:18 +0800

    添加欢迎界面

--- superwechat/src/cn/ucai/superwechat/ui/SplashActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/SplashActivity.java
@@ -26,6 +26,7 @@ public class SplashActivity extends BaseActivity {
 		RelativeLayout rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
 		TextView versionText = (TextView) findViewById(R.id.tv_version);
 
+
 //		versionText.setText(getVersion());
 //		AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
 //		animation.setDuration(1500);
@@ -66,7 +67,7 @@ public class SplashActivity extends BaseActivity {
 						Thread.sleep(sleepTime);
 					} catch (InterruptedException e) {
 					}
-					startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+					startActivity(new Intent(SplashActivity.this, GuideActivity.class));
 					finish();
 				}
 			}
