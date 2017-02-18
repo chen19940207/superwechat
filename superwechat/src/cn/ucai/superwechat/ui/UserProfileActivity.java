2dfc37141cb35d0e9a692c08a5bb63f22db23b7d
chenqian <1173006527@qq.com>
2017-02-18 14:17:31 +0800

    新建带头像的群组

--- superwechat/src/cn/ucai/superwechat/ui/UserProfileActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/UserProfileActivity.java
@@ -50,8 +50,7 @@ import cn.ucai.superwechat.utils.ResultUtils;
 
 public class UserProfileActivity extends BaseActivity implements OnClickListener {
     private static final String TAG = UserProfileActivity.class.getSimpleName();
-    private static final int REQUESTCODE_PICK = 1;
-    private static final int REQUESTCODE_CUTTING = 2;
+
     @BindView(R.id.img_back)
     ImageView imgBack;
     @BindView(R.id.txt_title)
@@ -150,7 +149,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
                             case 1:
                                 Intent pickIntent = new Intent(Intent.ACTION_PICK, null);
                                 pickIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-                                startActivityForResult(pickIntent, REQUESTCODE_PICK);
+                                startActivityForResult(pickIntent, I.REQUESTCODE_PICK);
                                 break;
                             default:
                                 break;
@@ -235,13 +234,13 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
-            case REQUESTCODE_PICK:
+            case I.REQUESTCODE_PICK:
                 if (data == null || data.getData() == null) {
                     return;
                 }
                 startPhotoZoom(data.getData());
                 break;
-            case REQUESTCODE_CUTTING:
+            case I.REQUESTCODE_CUTTING:
                 if (data != null) {
                     uploadAppUserAvatar(data);
                 }
@@ -262,7 +261,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
         intent.putExtra("outputY", 300);
         intent.putExtra("return-data", true);
         intent.putExtra("noFaceDetection", true);
-        startActivityForResult(intent, REQUESTCODE_CUTTING);
+        startActivityForResult(intent, I.REQUESTCODE_CUTTING);
     }
 
     /**
@@ -369,7 +368,6 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
 
         dialog.show();
     }
-
 
     public byte[] Bitmap2Bytes(Bitmap bm) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
